117b5d484971121e1d5ffa9692db44f4caca6cf0
hamdi zaghouan <141347852+hamdiz0@users.noreply.github.com>
2024-12-18 01:06:17 +0100

    Update test.py

--- train/test.py
+++ train/test.py
@@ -5,9 +5,9 @@ model = YOLO("yolo11n.pt")
 
 # Use the model
 test_results = model.predict(
-    source='test/images',
-    save=True,           # Save prediction visualizations
-    conf=0.30,           # Confidence threshold
-    save_txt=True,       # Save predictions as txt files
-    save_conf=True       # Save confidence scores
+    source='test/images',    # specify test set path
+    save=True,               # Save prediction visualizations
+    conf=0.30,               # Confidence threshold
+    save_txt=True,           # Save predictions as txt files
+    save_conf=True           # Save confidence scores
 )
