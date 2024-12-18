bffc97eb75e7cd044d86b66756565e8fd92f4064
hamdiz0 <hamdizaghouan21166@gmail.com>
2024-12-18 01:25:09 +0100

    updated

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 
 # Use the model
-results = model.train(data="./yaml-config/train.yaml", epochs=10)  # train the model
+results = model.train(data="./train.yaml", epochs=10)  # train the model
