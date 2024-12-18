7bbd7079093f3cb5a40a284158144e31f66d84d5
hamdi zaghouan <141347852+hamdiz0@users.noreply.github.com>
2024-12-18 01:09:18 +0100

    Update test.py

--- train/test.py
+++ train/test.py
@@ -1,13 +1,15 @@
 from ultralytics import YOLO
+import os
 
 # Load a model
-model = YOLO("yolo11n.pt")
+model_path = os.path.join('.', 'runs', 'detect', 'train3', 'weights', 'best.pt') # path to the trained model
+model = YOLO(model_path)
 
 # Use the model
 test_results = model.predict(
-    source='test/images',    # specify test set path
-    save=True,               # Save prediction visualizations
-    conf=0.30,               # Confidence threshold
-    save_txt=True,           # Save predictions as txt files
-    save_conf=True           # Save confidence scores
+    source="/win/Hamdi/ml/model/Dataset/test/images", # Test images
+    save=True,                                        # Save prediction visualizations
+    conf=0.25,                                        # Confidence threshold
+    save_txt=True,                                    # Save predictions as txt files
+    save_conf=True                                    # Save confidence scores
 )
